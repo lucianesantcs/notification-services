31f0a8acf526237444c2d9305ffda5d520ee28a4
Luciane Santos <lucianesantcs@gmail.com>
2022-12-27 18:43:36 -0300

    feat: criado método para criação de notificações

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
+import { randomUUID } from 'node:crypto';
 
 @Controller('notifications')
 export class AppController {
   constructor(private readonly prismaService: PrismaService) {}
 
   @Get()
-  getHello() {
+  list() {
     return this.prismaService.notification.findMany();
+  }
+
+  @Post()
+  async create() {
+    await this.prismaService.notification.create({
+      data: {
+        id: randomUUID(),
+        content: 'Você tem uma nova notificação de amizade!',
+        category: 'social',
+        recipientId: randomUUID(),
+      },
+    });
   }
 }
