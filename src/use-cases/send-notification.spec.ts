27ab6d2b9d5601ed698d3875fa041f19614a44d2
Luciane Santos <lucianesantcs@gmail.com>
2023-01-04 17:54:52 -0300

    feat: criado banco de dados in memory para testes

--- src/use-cases/send-notification.spec.ts
+++ src/use-cases/send-notification.spec.ts
@@ -1,26 +1,18 @@
-import { Notification } from 'src/application/entities/notification';
+import { InMemoryNotificationsRepository } from './../../test/repositories/in-memory-notifications-repository';
 import { SendNotification } from './send-notification';
-
-const notifications: Notification[] = [];
-
-const notificationsRepository = {
-  async create(notification: Notification) {
-    notifications.push(notification);
-  },
-};
 
 describe('Send notification', () => {
   it('should be able to send a notification', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
     const sendNotification = new SendNotification(notificationsRepository);
 
-    await sendNotification.execute({
+    const { notification } = await sendNotification.execute({
       category: 'social',
       content: 'Nova solicitação de amizade.',
       recipientId: 'example-recipient-id',
     });
 
-    console.log(notifications);
-
-    expect(notifications).toHaveLength(1);
+    expect(notificationsRepository.notifications).toHaveLength(1);
+    expect(notificationsRepository.notifications[0]).toEqual(notification);
   });
 });
