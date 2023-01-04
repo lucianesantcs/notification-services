5f10da24e92da1772249cfd2f6b5d2239487bc30
Luciane Santos <lucianesantcs@gmail.com>
2023-01-04 17:45:01 -0300

    feat: criado repository (middleware) de notificação e correção no teste

--- src/use-cases/send-notification.spec.ts
+++ src/use-cases/send-notification.spec.ts
@@ -1,15 +1,26 @@
+import { Notification } from 'src/application/entities/notification';
 import { SendNotification } from './send-notification';
+
+const notifications: Notification[] = [];
+
+const notificationsRepository = {
+  async create(notification: Notification) {
+    notifications.push(notification);
+  },
+};
 
 describe('Send notification', () => {
   it('should be able to send a notification', async () => {
-    const sendNotification = new SendNotification();
+    const sendNotification = new SendNotification(notificationsRepository);
 
-    const { notification } = await sendNotification.execute({
+    await sendNotification.execute({
       category: 'social',
       content: 'Nova solicitação de amizade.',
       recipientId: 'example-recipient-id',
     });
 
-    expect(notification).toBeTruthy();
+    console.log(notifications);
+
+    expect(notifications).toHaveLength(1);
   });
 });
--- src/use-cases/send-notification.ts
+++ src/use-cases/send-notification.ts
@@ -1,3 +1,4 @@
+import { NotificationsRepository } from 'src/application/repositories/notifications-repository';
 import { Content } from './../application/entities/content';
 import { Notification } from './../application/entities/notification';
 
@@ -12,6 +13,8 @@ interface SendNotificationResponse {
 }
 
 export class SendNotification {
+  constructor(private notificationsRepository: NotificationsRepository) {}
+
   async execute(
     request: SendNotificationRequest,
   ): Promise<SendNotificationResponse> {
@@ -22,6 +25,8 @@ export class SendNotification {
       content: new Content(content),
       category,
     });
+
+    await this.notificationsRepository.create(notification);
 
     return {
       notification,
