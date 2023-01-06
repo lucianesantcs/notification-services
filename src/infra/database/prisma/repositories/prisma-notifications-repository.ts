5632de26bc4c15346711ac9b3e322521ab951435
Luciane Santos <lucianesantcs@gmail.com>
2023-01-06 17:17:39 -0300

    feat: adicionado decorator Injectable na classe prisma notifications repository

--- src/infra/database/prisma/repositories/prisma-notifications-repository.ts
+++ src/infra/database/prisma/repositories/prisma-notifications-repository.ts
@@ -1,7 +1,9 @@
+import { Injectable } from '@nestjs/common';
 import { PrismaService } from './../prisma.service';
 import { Notification } from '../../../../application/entities/notification';
 import { NotificationsRepository } from '../../../../application/repositories/notifications-repository';
 
+@Injectable()
 export class PrismaNotificationsRepository implements NotificationsRepository {
   constructor(private prismaService: PrismaService) {}
 
