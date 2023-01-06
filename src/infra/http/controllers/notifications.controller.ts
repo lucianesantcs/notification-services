d868bda3fb6739f17cd4c93ae9267ec6dbee4465
Luciane Santos <lucianesantcs@gmail.com>
2023-01-06 17:05:28 -0300

    refactor: arrumado import em notifications controller

--- src/infra/http/controllers/notifications.controller.ts
+++ src/infra/http/controllers/notifications.controller.ts
@@ -1,10 +1,23 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { SendNotification } from './../../../use-cases/send-notification';
+import { Body, Controller, Post } from '@nestjs/common';
 import { CreateNotificationBody } from '../dtos/create-notification-body';
 
 @Controller('notifications')
 export class NotificationsController {
+  constructor(private sendNotification: SendNotification) {}
+
   @Post()
   async create(@Body() body: CreateNotificationBody) {
     const { content, category, recipientId } = body;
+
+    const { notification } = await this.sendNotification.execute({
+      recipientId,
+      content,
+      category,
+    });
+
+    return {
+      notification,
+    };
   }
 }
