4baddc25f948884a53ca6bda9494d9f30dcec552
Luciane Santos <lucianesantcs@gmail.com>
2023-01-09 16:46:21 -0300

    feat: adicionado na tabela o campo canceledAt

--- src/application/entities/notification.ts
+++ src/application/entities/notification.ts
@@ -7,6 +7,7 @@ export interface NotificationProps {
   content: Content;
   category: string;
   readAt?: Date | null;
+  canceledAt?: Date | null;
   createdAt: Date;
 }
 
@@ -61,6 +62,15 @@ export class Notification {
 
   public get readAt(): Date | null | undefined {
     return this.props.readAt;
+  }
+
+  // canceledAt
+  public cancel() {
+    this.props.canceledAt = new Date();
+  }
+
+  public get canceledAt(): Date | null | undefined {
+    return this.props.canceledAt;
   }
 
   // createdAt
