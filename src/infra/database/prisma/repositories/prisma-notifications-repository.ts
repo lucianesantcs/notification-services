7ac1b49318004447296467d31f1e1f67f88cde4f
Luciane Santos <lucianesantcs@gmail.com>
2023-01-09 15:40:37 -0300

    refactor: configurado typescript paths em alguns arquivos

--- src/infra/database/prisma/repositories/prisma-notifications-repository.ts
+++ src/infra/database/prisma/repositories/prisma-notifications-repository.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from './../prisma.service';
-import { Notification } from '../../../../application/entities/notification';
-import { NotificationsRepository } from '../../../../application/repositories/notifications-repository';
+import { Notification } from '@application/entities/notification';
+import { NotificationsRepository } from '@application/repositories/notifications-repository';
 
 @Injectable()
 export class PrismaNotificationsRepository implements NotificationsRepository {
