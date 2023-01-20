f0fbe95b9ba0495ac0dc3b323883973a1efdcbe9
Luciane Santos <lucianesantcs@gmail.com>
2023-01-20 15:24:47 -0300

    feat: criado cancel notification e testes

---
+++ src/use-cases/cancel-notification.ts
@@ -0,0 +1,31 @@
+import { NotificationNotFound } from './errors/notification-not-found';
+import { Injectable } from '@nestjs/common';
+import { NotificationsRepository } from '../application/repositories/notifications-repository';
+
+interface CancelNotificationRequest {
+  notificationId: string;
+}
+
+type CancelNotificationResponse = void;
+
+@Injectable()
+export class CancelNotification {
+  constructor(private notificationsRepository: NotificationsRepository) {}
+
+  async execute(
+    request: CancelNotificationRequest,
+  ): Promise<CancelNotificationResponse> {
+    const { notificationId } = request;
+
+    const notification = await this.notificationsRepository.findById(
+      notificationId,
+    );
+
+    if (!notification) {
+      throw new NotificationNotFound();
+    }
+
+    notification.cancel();
+    await this.notificationsRepository.save(notification);
+  }
+}
